672acab60a75686099a176d183a65c5813d5f4b8
Kevin Miller <100856999+kevinmilleryo@users.noreply.github.com>
2023-07-03 12:55:49 -0400

    added text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
-export function About(){
-    return <h1>About</h1>
+export function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Brewery store cart made using React & Typescript.</p>
+        </>
+    )
 }
